a470da470360437381afdb109a1bc04e25c30bb9
cdecide <cdecide@gmail.com>
2016-02-13 17:36:45 +0900

    2/13 lastest

--- guestbook/models.py
+++ guestbook/models.py
@@ -9,3 +9,6 @@ class Post(models.Model):
 	username = models.CharField(max_length=40)
 	content = models.TextField()
 	create_at = models.DateTimeField(auto_now_add=True)
+
+	def __str__(self):
+		return '{date} 만들어진 {number} 번 글'.format(date=self.create_at, number=self.pk)
--- guestbook/views.py
+++ guestbook/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Post
+
 def hello(request):
 	msg = 'my name is cdecide'
 	return render(request,'hello.html',{'message':msg})
